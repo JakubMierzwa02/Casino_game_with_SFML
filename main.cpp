0709d6a2239a2ab5ac205f5d2dcd3508c8ed80f4
Jakub Mierzwa <kontakt.jakubmierzwa@gmail.com>
2023-02-28 10:38:17 +0100

    SFML library added

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
+#include "SFML/Graphics.hpp"
+#include "SFML/Window.hpp"
+#include "SFML/System.hpp"
+
 int main()
 {
+	sf::RenderWindow window(sf::VideoMode(1024, 1024), "Casino game");
 
+	window.clear();
+
+	window.display();
 
 	return 0;
 }
